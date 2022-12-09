bd09ec3b08f228bad03ca924283f3cd96c0a584d
Bright Anua <futurecantellu@yahoo.com>
2022-12-09 00:23:54 -0800

    Corrections

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda r: list(map(lambda x: x**2, r)), matrix)))
+def weight_average(my_list=[]):
+    if my_list == [] or my_list is None:
+        return (0)
+    res = 0
+    res2 = 0
+    for x, y in my_list:
+        res += x * y
+        res2 += y
+    return (res / res2)
