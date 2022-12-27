10c40a348d28e2fc2dbe83e274ff3aa613e3b2df
Bright Anua <futurecantellu@yahoo.com>
2022-12-26 22:51:40 -0800

    Print x elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-j = 0
-for i in range(0, x):
-try:
-my_list[i]
-except IndexError:
-print(end='')
-else:
-j += 1
-print(my_list[i], end='')
-print()
-return j
+    cmpt = 0
+    for index in range(x):
+        try:
+            print(my_list[index], end='')
+            cmpt += 1
+        except Exception as error:
+            break
+    print('')
+    return cmpt 
--- 0x05-python-exceptions/1-safe_print_integer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-	def safe_print_integer(value):
-	is_int = True
-	try:
-	print("{:d}".format(value))
-	except (ValueError, TypeError):
-	is_int = False
-	return is_int
